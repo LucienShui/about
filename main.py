186433c2356935184ff47347ca3a7607ea7ca0b5
Lucien <lucien@lucien.ink>
2022-04-03 23:34:22 +0800

    add reload

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/reload', methods=['POST'])
+def reload() -> Response:
+    bot.knowledge_list = bot.load_knowledge(bot.corpus_base_dir)
+    return jsonify({'code': 200})
+
+
 @app.route('/chat', methods=['POST'])
 def chat() -> Response:
     text = request.json['text']
